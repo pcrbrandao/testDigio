f3ca968f20ebb4a7f23cd33b7b685d87e01a8e33
Pedro Brandao <pcrbrandao@gmail.com>
2024-10-11 17:40:15 -0300

    build: criando o projeto

---
+++ TestDigio/TestDigio/TestDigioApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestDigioApp.swift
+//  TestDigio
+//
+//  Created by Pedro Brandão on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TestDigioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
